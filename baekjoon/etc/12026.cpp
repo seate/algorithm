17286ebafb04dcdf42ece070355f8b8463a70c16
siwoo <k74415834@gmail.com>
2024-11-18 01:54:38 +0900

    12026번

--- baekjoon/etc/12026.cpp
+++ baekjoon/etc/12026.cpp
@@ -7,35 +7,54 @@
 #include <limits.h>
 using namespace std;
 
+#define MAX 10000000
+
+vector<char> c({'B', 'O', 'J'});
+unordered_map<char, int> boj = {{'B', 0}, {'O', 1}, {'J', 2}};
+
+char getBefore(char cur) {
+    int i = boj[cur] - 1;
+    if (i < 0) i = boj.size() - 1;
+
+    return c[i];
+}
+
 int main()
 {
-    int N, lastB = 0, lastO = -1, lastJ = -1;
+    int N;
     string s;
-
     scanf("%d", &N);
-    scanf("%s", s);
-
-    vector<int> dp(N, INT_MAX);
-
-    unordered_map<char, int> boj;
-
-    boj['B'] = 0;
-    boj['O'] = 1;
-    boj['J'] = 2;
+    cin >> s;
+    
+    //*
+    vector<int> dp(N, MAX);
+    dp[0] = 0;
 
 
-
-
-    char curChar;
     for (int i = 1; i < N; i ++) {
-        curChar = s[i];
+        char cur = s[i];
+        char before = getBefore(cur);
+        for (int j = 0; j < i; j++) {
+            if (s[j] == before) {
+                dp[i] = min(dp[i], dp[j] + (i - j) * (i - j));
+            }
+        }
+    }
+    
 
-        
+    /*
+    for (int d : dp) {
+        printf("%d ", d);
+    }
+    printf("\n");
+    //*/
 
-        
-        
-        
-
+    if (dp.back() == MAX) {
+        printf("-1");
+    }
+    else {
+        printf("%d", dp.back());
     }
 
+//*/
 }
